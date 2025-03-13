ef9304465c85d64778178419ee995db9568dd867
Jeferson Gonçalves <jeferson.gonssalves.alcantara@gmail.com>
2025-03-13 04:44:26 +0000

    Correção desafio novato Super_trunfo

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -8,8 +8,8 @@ int main(){
 
 
     char estado_A = 'A';
-    char codigo_da_carta_A = "A01";
-    char nome_da_cidade_A = "Alagoas";
+    char codigo_da_carta_A = 'A01';
+    char nome_da_cidade_A = 'Alagoas';
     int população_A = 3220104; //Aproximada em 2024
     float area_em_km²_A = 27.230; 
     float pib_A = 76.07; // PIB Em bilhões de reais
@@ -18,15 +18,15 @@ int main(){
 
     //Cadastro da segunda carta cidade Bahia dados de 2024
     char estado_B ='B';
-    char codigo_da_carta_B = "B01";
-    char nome_da_cidade_B = "Bahia";
+    char codigo_da_carta_B = 'B01';
+    char nome_da_cidade_B = 'Bahia';
     int população_B = 14850513; //Aproximada em 2024
     float area_em_km²_B = 567.295; 
     float pib_B = 349.00; // PIB Em bilhões de reais
     int número_de_pontos_turisticos_B = 29; //fonte tirada do google
 
     //Dados da primeira carta
-    printf("Carta_1:\n");
+    scanf("Carta_1:\n");
     printf("Estado: %c\n", estado_A);
     printf("Código: %s\n", codigo_da_carta_A);
     printf("Nome da Cidade: %s\n", nome_da_cidade_A);
